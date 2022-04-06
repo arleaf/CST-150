209ded717744aa86457e74579cc294fcc3d37a15
Arleth <62979596+arleaf@users.noreply.github.com>
2022-04-05 21:24:24 -0700

    Add files via upload

---
+++ Week5/Activity9/MethodStubs.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity._9
+{
+    internal class MethodStubs
+    {
+        //Arleth Martinez Activity 9
+
+        //Methods1
+        //1. Write a method that takes two integers and displays their sum.
+        public void displaySum(int num1, int num2) { }
+        //2. Write a method that takes five doubles and returns their average.
+        public double findAverage(double num1, double num2, double num3, double num4, double num5) { }
+        //3. Write a method that returns the sum of two randomly generated integers.
+        public int randomSum() { }
+        //4. Write a method that takes three integers and returns true if their sum is divisible by 3, false otherwise.
+        public bool divisibleBy3(int num1, int num2, int num3) { }
+        //5.Write a method that takes two strings and displays the string that has fewer characters.
+        public void displayFewerString(string str1, string str2) { }
+        //6.Write a method that takes an array of doubles and returns the largest value in the array.
+        public double largestValue(double[] doubArray) { }
+        //7.Write a method that generates and returns an array of fifty integer values.
+        public double[] generateArray() { }
+        //8.Write a method that takes two bool variables and returns true if they have the same value, false otherwise.
+        public bool isSameValue(bool bool1, bool bool2) { }
+        //9.Write a method that takes an int and a double and returns their product.
+        public double multiply(int num1, double num2) { }
+        //10.Write a method that takes a two-dimensional array of integers and returns the average of the entries.
+        public double averageOf2dArray(int[,] array) { }
+    }
+}
