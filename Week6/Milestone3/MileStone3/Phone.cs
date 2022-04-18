7c4b67c10cf07b66befbc6dc8bcc1d5a0fc689b2
Arleth <62979596+arleaf@users.noreply.github.com>
2022-04-17 21:15:02 -0700

    Milestone 3, CST150

---
+++ Week6/Milestone3/MileStone3/Phone.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MileStone3
+{
+     class Phone
+    {
+        //attributes of Phone
+            string name;
+            string color;
+            string storage;
+            string brand;
+            string carrier;
+            int quantity;
+
+            //Constructor
+            public Phone(string name,string color, string storage, string brand, string carrier)
+            {
+            this.name = name;
+                this.color = color;
+                this.storage = storage;
+                this.brand = brand;
+                this.carrier = carrier;
+            }
+
+            // Get and Set Methods
+            public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+            public string Color
+            {
+                get { return color; }
+                set { color = value; }
+            }
+            public string Storage
+            {
+                get { return storage; }
+                set { storage = value; }
+            }
+            public string Brand
+            {
+                get { return brand; }
+                set { brand = value; }
+            }
+            public string Carrier
+            {
+                get { return carrier; }
+                set { carrier = value; }
+            }
+        public int Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+            //Calculates total Price including shipping and taxes rounded by 2
+            public double calculateTotalPrice(double basePrice = 589.89, double taxRate = 0.15, double shippingPrice = 15.00)
+            {
+                double taxPrice = basePrice * taxRate;
+                return Math.Round(basePrice + taxPrice + shippingPrice, 2);
+            }
+
+            //Displays Phone Details
+            public void displayPhoneDetails()
+            {
+                Console.WriteLine("Brand: " + brand + " Storage: " + storage + " Color: " + color + " Carrier: " + carrier);
+            }
+     }
+}
+
---
+++ Week6/Milestone3/MileStone3/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//Create Inventory Manager
+namespace MileStone3
+{
+    class Program
+    {
+        static public void Main(String[] args)
+        {
+            //Create 3 phone objects
+            Phone iphone11 = new Phone("iphone11","Purple","256 GB","apple","Tmoblie");
+            Phone samsung = new Phone("samsungNote","Black", "128 GB", "Samsung", "AT&T");
+            Phone stylo5 = new Phone("Stylo5","Gray", "64 GB", "LG", "MetroPCS");
+            //Create Inventory Manager
+            InventoryManager inventoryManager = new InventoryManager(iphone11);
+
+            //*TESTING THE CLASS METHODS
+            //Before Calling Add method
+            Console.WriteLine("Before adding");
+            inventoryManager.displayInventoryManager();
+            //After Adding the objects to inventory
+            Console.WriteLine("");
+            Console.WriteLine("After adding");
+            inventoryManager.addInventory(samsung);
+            inventoryManager.addInventory(stylo5);
+            inventoryManager.displayInventoryManager();
+            //Removing An object
+            Console.WriteLine("");
+            Console.WriteLine("Before Removing:");
+            inventoryManager.displayInventoryManager();
+            inventoryManager.removeIventory(samsung);
+            Console.WriteLine("");
+            Console.WriteLine("After Removing");
+            inventoryManager.displayInventoryManager();
+            //Restocking a Phone item
+            Console.WriteLine("");
+            Console.WriteLine("Quantity Before Restock:" + iphone11.Quantity.ToString());
+            inventoryManager.restockInventory(iphone11, 5);
+            inventoryManager.restockInventory(iphone11, 10);
+            Console.WriteLine("");
+            Console.WriteLine("Quantity after restock:" + iphone11.Quantity.ToString());
+            //Searching for a phone item
+            Console.WriteLine("");
+            Console.WriteLine("Searching for " + stylo5.Name);
+            Console.WriteLine("Found: "+ inventoryManager.search(stylo5.Brand, stylo5.Color).Name);
+        }
+    }
+}
+
